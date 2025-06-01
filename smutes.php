8783a72266370786f642cc5079a390d52d0d7d80
hydnbyte <daffisyahla08@gmail.com>
2025-06-02 06:42:02 +0700

    Update smutes.php

--- smutes.php
+++ smutes.php
@@ -13,6 +13,10 @@ $data = json_decode(file_get_contents("php://input"), true);
 
 foreach ($data as $item) {
     $mysqli->query("UPDATE smutes SET stok = stok - 1 WHERE id = {$item['id']}");
+
+    $stmt = $mysqli->prepare("INSERT INTO history (item_id, nama_barang, harga) VALUES (?, ?, ?)");
+    $stmt->bind_param("isi", $item['id'], $item['name'], $item['price']);
+    $stmt->execute();
 }
 ?>
 -->
