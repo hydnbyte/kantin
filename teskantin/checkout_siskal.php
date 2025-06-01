b1f54e110ef38a294a0ea31eb94a8eb965335459
hydnbyte <daffisyahla08@gmail.com>
2025-06-02 01:07:31 +0700

    Update checkout_siskal.php

--- teskantin/checkout_siskal.php
+++ teskantin/checkout_siskal.php
@@ -4,6 +4,6 @@ $mysqli = new mysqli("localhost", "root", "", "kantin2");
 $data = json_decode(file_get_contents("php://input"), true);
 
 foreach ($data as $item) {
-    $mysqli->query("UPDATE smutes SET stok = stok - 1 WHERE id = {$item['id']}");
+    $mysqli->query("UPDATE siskal SET stok = stok - 1 WHERE id = {$item['id']}");
 }
 ?>
